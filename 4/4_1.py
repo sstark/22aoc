65a2d36cd1df37e6e7eb41ae218969c56ed7faf5
Sebastian Stark <sstark@mailbox.org>
2022-12-04 08:13:39 +0100

    day4 #1

---
+++ 4/4_1.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+class Sections():
+
+    def __init__(self, s):
+        a, b = s.split("-")
+        self.id_lo, self.id_hi = int(a), int(b)
+
+    def contains(self, s):
+        return s.id_lo >= self.id_lo and s.id_hi <= self.id_hi
+
+    def __repr__(self):
+        return f"{self.id_lo}-{self.id_hi}"
+
+class Pair():
+
+    def __init__(self, s):
+        super().__init__()
+        a, b = s.split(",")
+        self.p1 = Sections(a)
+        self.p2 = Sections(b)
+
+    def check(self):
+        return self.p1.contains(self.p2) or self.p2.contains(self.p1)
+
+    def __repr__(self):
+        return f"{self.p1},{self.p2}"
+
+s = 0
+with open("input", "r") as f:
+    for l in f:
+        # print(l)
+        p = Pair(l.strip())
+        if p.check(): s+=1
+
+print(s)
