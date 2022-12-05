7f7de3be131b82a646350e4faafb59daef5a6cac
Sebastian Stark <sstark@mailbox.org>
2022-12-05 07:23:47 +0100

    day 5 #1

---
+++ 5/5_1.py
@@ -0,0 +1,70 @@
+#!/bin/python
+
+class Crates():
+    def __init__(self, st):
+        self.crates = st
+
+    def get_last(self):
+        return self.crates[-1]
+
+    def pop(self):
+        return self.crates.pop()
+
+    def push(self, n):
+        self.crates.append(n)
+
+    def __repr__(self):
+        return f"{self.crates}"
+
+
+class CrateManager():
+    def __init__(self, piles):
+        self.stacks = []
+        for s in piles:
+            c = Crates(list(s))
+            self.stacks.append(c)
+
+    def move_one(self, fr, to):
+        self.stacks[to].push(self.stacks[fr].pop())
+
+    def move(self, count, fr, to):
+        # move 8 from 3 to 2
+        for _ in range(count):
+            self.move_one(fr, to)
+
+    def __repr__(self):
+        return f"{self.stacks}"
+
+
+STACKS_INIT = [
+    "HBVWNMLP",
+    "MQH",
+    "NDBGFQML",
+    "ZTFQMWG",
+    "MTHP",
+    "CBMJDHGT",
+    "MNBFVR",
+    "PLHMRGS",
+    "PDBCN",
+]
+
+STACKS_INIT_TEST = [
+    "ZN", "MCD", "P"
+]
+
+cm = CrateManager(STACKS_INIT)
+print(cm)
+with open("input", "r") as f:
+    for l in f:
+        if l.startswith("move"):
+            l = l.split(" ")
+            cnt = int(l[1])
+            fr = int(l[3]) - 1
+            to = int(l[5]) - 1
+            cm.move(cnt, fr, to)
+
+print(cm)
+l = []
+for s in cm.stacks:
+    l.append(s.get_last())
+print("".join(l))
