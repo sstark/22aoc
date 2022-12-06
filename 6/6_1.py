cd092b6594fa66fa5f7dc50ebb5b4c057887b63f
Sebastian Stark <sstark@mailbox.org>
2022-12-06 06:50:44 +0100

    day 6 #1

---
+++ 6/6_1.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+class RingBuf():
+    def __init__(self, size):
+        self.buf = []
+        self.size = size
+        self.count = 0
+
+    def put(self, e):
+        self.buf.append(e)
+        self.count += 1
+        if len(self.buf) > self.size:
+            self.buf.pop(0)
+
+    def check_diff(self):
+        if len(self.buf) == self.size:
+            return len(set(self.buf)) == len(self.buf)
+        else:
+            return False
+            
+
+r = RingBuf(4)
+with open("input", "r") as f:
+    for c in f.read():
+        if c != '\n':
+            r.put(c)
+            if r.check_diff():
+                print(r.count)
+                break
